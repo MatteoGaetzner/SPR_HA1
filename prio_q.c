fdf490088a9e25c25e8f4636546614625d70a8cc
Matteo Gätzner <m.gatzner@campus.tu-berlin.de>
2020-05-04 18:54:46 +0200

    initialization

---
+++ prio_q.c
@@ -0,0 +1,31 @@
+#include "prio_q.h"
+
+struct prio_q * prio_q_create()
+{
+	// TODO
+}
+
+void prio_q_enqueue(struct prio_q *q, void *data, int prio)
+{
+	// TODO
+}
+
+void * prio_q_dequeue(struct prio_q *q)
+{
+	// TODO
+}
+
+void * prio_q_front(struct prio_q *q)
+{
+	// TODO
+}
+
+int prio_q_destroy(struct prio_q *q, void ** data)
+{
+	// TODO
+}
+
+void prio_q_print(struct prio_q * q, void (*print_data)(void*))
+{
+	// Implementierung dieser Funktion ist OPTIONAL und wird daher NICHT BEWERTET!
+}
